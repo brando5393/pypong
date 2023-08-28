11ca91e3778686a3647ac8effb9849bae4fb0653
Brandon Williams <brando5393@gmail.com>
2023-08-27 19:16:01 -0500

    inital ball movement working

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ pygame.init()
 # sizes and positions
 WINDOW_HEIGHT, WINDOW_WIDTH = 600, 1000
 PADDLE_HEIGHT, PADDLE_WIDTH = 120, 20
+# ball values
 ball_radius = 15
 ball_x, ball_y = WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2
+ball_speed_x, ball_speed_y = 1, 1
+# paddle values
 left_paddle_y = right_paddle_y = WINDOW_HEIGHT // 2 - PADDLE_HEIGHT // 2
 left_paddle_x = 100 - PADDLE_WIDTH // 2
 right_paddle_x = WINDOW_WIDTH - (100 - PADDLE_WIDTH // 2)
 
 # Colors
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 VIOLET = (148, 0, 211)
 INDIGO = (75, 0, 130)
 BLUE = (0, 0, 255)
@@ -44,9 +48,15 @@ game_is_running = True
 
 # main game loop
 while game_is_running:
+    window.fill(BLACK)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_is_running = False
+    # movement
+
+    # OBJECTS
+    ball_x += ball_speed_x
+    ball_y += ball_speed_y
     pygame.draw.circle(window, ball_color, (ball_x, ball_y), ball_radius)
     pygame.draw.rect(window, paddle_color, pygame.Rect(
         left_paddle_x, left_paddle_y, PADDLE_WIDTH, PADDLE_HEIGHT))
